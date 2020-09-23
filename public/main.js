94b0685347e1513cf11a67dc4d2f9ec99c4bbd5f
Isa Conteh <iconteh21@gmail.com>
2020-09-22 20:30:07 -0400

    added client side code for deletes

--- public/main.js
+++ public/main.js
@@ -1,4 +1,5 @@
 const update = document.querySelector('#update-button');
+const deleteButton = document.querySelector('#delete-button');
 
 update.addEventListener('click', _=> {
     fetch('/quotes', {
@@ -10,3 +11,19 @@ update.addEventListener('click', _=> {
         })
     });
 });
+
+deleteButton.addEventListener('click', _=> {
+    fetch('/quotes', {
+        method: 'delete',
+        headers: {'Content-Type': 'application'},
+        body: JSON.stringify({
+            name: 'Darth Vadar'
+        })
+    })
+    .then(res => {
+        if(res.ok) return res.json()
+    })
+    .then(data => {
+        window.location.reload();
+    })
+})
